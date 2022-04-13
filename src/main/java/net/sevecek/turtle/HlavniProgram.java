5612754052d1cd893d4022255e7f18068997064a
Lenka Dosedělová <dosedelova.l@seznam.cz>
2022-04-13 19:51:26 +0200

    Domeček 2.verze pomocí metody

--- src/main/java/net/sevecek/turtle/HlavniProgram.java
+++ src/main/java/net/sevecek/turtle/HlavniProgram.java
@@ -3,26 +3,44 @@ package net.sevecek.turtle;
 import net.sevecek.turtle.engine.*;
 
 public class HlavniProgram {
-
-    public void main(String[] args) {
         Turtle zofka;
 
-        zofka = new Turtle();
+    public void main(String[] args) {
 
+        zofka = new Turtle();
+        nakresliDomecek();
+
+
+    }
+
+    private void nakresliDomecek() {
+        nakresliCtverec();
+        zofka.move(100);
+        nakresliTrojuhelnik();
+        zofka.move(100);
+        zofka.turnLeft(90);
+        zofka.move(100);
+        zofka.penUp();
+        zofka. move(200);
+        zofka.penDown();
+        zofka.turnLeft(90);
+    }
+
+    public void  nakresliCtverec(){
         for (int i = 0; i < 4 ; i++) {
             zofka.move(100);
             zofka.turnLeft(90);
         }
+    }
 
-        zofka.move(100);
-
+    public void nakresliTrojuhelnik(){
         zofka.turnLeft(30);
         zofka.move(100);
         zofka.turnLeft(120);
         zofka.move(100);
         zofka.turnLeft(30);
-
     }
+
 
 
 }
