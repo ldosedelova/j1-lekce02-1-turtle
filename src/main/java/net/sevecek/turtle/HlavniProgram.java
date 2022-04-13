44d9705e3fd76840e9665628ff394157d6511958
Lenka Dosedělová <dosedelova.l@seznam.cz>
2022-04-13 19:35:27 +0200

    Domeček 1.verze

--- src/main/java/net/sevecek/turtle/HlavniProgram.java
+++ src/main/java/net/sevecek/turtle/HlavniProgram.java
@@ -8,6 +8,21 @@ public class HlavniProgram {
         Turtle zofka;
 
         zofka = new Turtle();
+
+        for (int i = 0; i < 4 ; i++) {
+            zofka.move(100);
+            zofka.turnLeft(90);
+        }
+
+        zofka.move(100);
+
+        zofka.turnLeft(30);
+        zofka.move(100);
+        zofka.turnLeft(120);
+        zofka.move(100);
+        zofka.turnLeft(30);
+
     }
+
 
 }
